c08e0015ede9ae792bcb92c92a72e56e89b68084
Po To Chan <potochan0330@gmail.com>
2022-03-30 12:16:27 +0800

    update for the api call interact with the publisher

--- src/main/java/com/poto/stock/StockController.java
+++ src/main/java/com/poto/stock/StockController.java
@@ -1,20 +1,13 @@
 package com.poto.stock;
 
-import com.poto.stock.entity.Options;
-import com.poto.stock.entity.Stock;
+import com.poto.stock.consumer.CustomGateway;
 import com.poto.stock.repository.OptionsRepository;
 import com.poto.stock.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class StockController {
@@ -25,36 +18,17 @@ public class StockController {
     @Autowired
     OptionsRepository optionsRepository;
 
+    @Autowired
+    CustomGateway customGateway;
+
     @GetMapping("/stockReport")
     public String getStockReport() {
-        List<String[]> dataLines = new ArrayList<>();
-        Double totalNav = 0.0;
-        DecimalFormat df = new DecimalFormat("#.####");
-        df.setRoundingMode(RoundingMode.HALF_UP);
-
-        File csvOutputFile = new File("profolio.txt");
-        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            List<Stock> stocks = stockRepository.findAll();
-            pw.format("%15s %15s %15s %15s %15s %15s %n", "stock code", "stock type", "price", "quantity", "position", "market value");
-            for (Stock stock : stocks) {
-                pw.format("%15s %15s %15s %15s %15s %15s %n", stock.getCode(), "common stock", stock.getLatest_price(),
-                        stock.getQuantity(), stock.getPosition() == 1 ? "Long" : "Short",
-                        df.format(stock.getLatest_price() * stock.getQuantity() * stock.getPosition()));
-                totalNav += stock.getLatest_price() * stock.getQuantity() * stock.getPosition();
-            }
-            List<Options> options = optionsRepository.findAll();
-            for (Options option : options) {
-                pw.format("%15s %15s %15s %15s %15s %15s %n", option.getCode(),
-                        option.getType() == 1 ? "Call Option" : "Put Option", option.getOption_price(),
-                        option.getQuantity(), option.getPosition() == 1 ? "Long" : "Short",
-                        df.format(option.getOption_price() * option.getQuantity() * option.getPosition()));
-                totalNav += option.getOption_price() * option.getQuantity() * option.getPosition();
-            }
-            pw.format("%15s %15s %15s %15s %15s %15s %n", "", "", "", "", "NAV:", df.format(totalNav));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Message<String> message = MessageBuilder.withPayload("report").build();
+        Message<String> result = customGateway.print(message);
+        if (result.getPayload().contains("filepath")) {
+            return "File path: " + result.getPayload().split(":")[1] + "\n";
         }
-        return "Exported file: " + csvOutputFile.getAbsolutePath() + " \n";
+        return "No filepath\n";
     }
 
 }
--- src/main/java/com/poto/stock/consumer/NavConsumer.java
+++ src/main/java/com/poto/stock/consumer/NavConsumer.java
@@ -10,6 +10,9 @@ import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.List;
@@ -25,6 +28,16 @@ public class NavConsumer {
 
     @ServiceActivator(inputChannel = "stockChannel")
     public Message<String> consumeMessage(Message<String> message) {
+        if(message.getPayload().equals("report")){
+            String filepath = exportReport();
+            return MessageBuilder.withPayload("filepath:"+filepath).build();
+        }else{
+            printConsole();
+            return MessageBuilder.withPayload("Message received").build();
+        }
+    }
+
+    private void printConsole(){
         DecimalFormat df = new DecimalFormat("#.####");
         df.setRoundingMode(RoundingMode.HALF_UP);
         Double total = 0.0;
@@ -50,7 +63,35 @@ public class NavConsumer {
 
         System.out.format("%15s %15s %15s %15s %15s %15s %n", "", "", "", "", "NAV:", df.format(total));
         System.out.println("---------------------------------------------------------------------------------------------");
-        return MessageBuilder.withPayload("Message received").build();
     }
 
+    private String exportReport(){
+        Double totalNav = 0.0;
+        DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        File csvOutputFile = new File("profolio.txt");
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            List<Stock> stocks = stockRepository.findAll();
+            pw.format("%15s %15s %15s %15s %15s %15s %n", "stock code", "stock type", "price", "quantity", "position", "market value");
+            for (Stock stock : stocks) {
+                pw.format("%15s %15s %15s %15s %15s %15s %n", stock.getCode(), "common stock", stock.getLatest_price(),
+                        stock.getQuantity(), stock.getPosition() == 1 ? "Long" : "Short",
+                        df.format(stock.getLatest_price() * stock.getQuantity() * stock.getPosition()));
+                totalNav += stock.getLatest_price() * stock.getQuantity() * stock.getPosition();
+            }
+            List<Options> options = optionsRepository.findAll();
+            for (Options option : options) {
+                pw.format("%15s %15s %15s %15s %15s %15s %n", option.getCode(),
+                        option.getType() == 1 ? "Call Option" : "Put Option", option.getOption_price(),
+                        option.getQuantity(), option.getPosition() == 1 ? "Long" : "Short",
+                        df.format(option.getOption_price() * option.getQuantity() * option.getPosition()));
+                totalNav += option.getOption_price() * option.getQuantity() * option.getPosition();
+            }
+            pw.format("%15s %15s %15s %15s %15s %15s %n", "", "", "", "", "NAV:", df.format(totalNav));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return csvOutputFile.getAbsolutePath();
+    }
 }
